fa97930898060596a365790f680608d35830a320
Chakri <chakripavani2002@gmail.com>
2025-11-05 19:38:19 +0530

    Updated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,7 +8,7 @@ import cors from "cors";
 //dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 import corsOptions from "./config/corsOption.js";
 //new Imports
 
